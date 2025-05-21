f8d51d0a9b3af77f6b4b00360c3ecca1b3304326
hf-chow <honfungchow@gmail.com>
2025-05-21 14:20:32 +0800

    add OpenAI response support

--- internal/model/data.go
+++ internal/model/data.go
@@ -7,41 +7,46 @@ type Model struct {
 }
 
 type OllamaModelRequest struct {
-    Model               string          `json:"model"`
-    Prompt              string          `json:"prompt"`
-    Stream              bool            `json:"stream"`
+    Model               string                      `json:"model"`
+    Prompt              string                      `json:"prompt"`
+    Stream              bool                        `json:"stream"`
 }
 
 type OllamaModelResponse struct {
-	Model               string          `json:"model"`
-	CreatedAt           string          `json:"created_at"`
-	Response            string          `json:"response"`
-	Done                bool            `json:"done"`
-	Context             []int           `json:"context"`
-	TotalDuration       int             `json:"total_duration"`
-	LoadDuration        int             `json:"load_duration"`
-	PromptEvalCount     int             `json:"prompt_eval_count"`
-	PromptEvalDuration  int             `json:"prompt_eval_duration"`
-	EvalCount           int             `json:"eval_count"`
-	EvalDuration        int             `json:"eval_duration"`
+	Model               string                      `json:"model"`
+	CreatedAt           string                      `json:"created_at"`
+	Response            string                      `json:"response"`
+	Done                bool                        `json:"done"`
+	Context             []int                       `json:"context"`
+	TotalDuration       int                         `json:"total_duration"`
+	LoadDuration        int                         `json:"load_duration"`
+	PromptEvalCount     int                         `json:"prompt_eval_count"`
+	PromptEvalDuration  int                         `json:"prompt_eval_duration"`
+	EvalCount           int                         `json:"eval_count"`
+	EvalDuration        int                         `json:"eval_duration"`
 }
 
 type OpenAIModelRequest struct {
-    Model               string          `json:"model"`
-    Prompt              string          `json:"prompt"`
+    Model               string                      `json:"model"`
+    Prompt              string                      `json:"prompt"`
 }
 
 type OpenAIModelResponse struct {
-	Model               string          `json:"model"`
-	CreatedAt           string          `json:"created_at"`
-	Response            string          `json:"response"`
-	Done                bool            `json:"done"`
-	Context             []int           `json:"context"`
-	TotalDuration       int             `json:"total_duration"`
-	LoadDuration        int             `json:"load_duration"`
-	PromptEvalCount     int             `json:"prompt_eval_count"`
-	PromptEvalDuration  int             `json:"prompt_eval_duration"`
-	EvalCount           int             `json:"eval_count"`
-	EvalDuration        int             `json:"eval_duration"`
+    ID                  string                      `json:"id"`
+	Model               string                      `json:"model"`
+	CreatedAt           string                      `json:"created_at"`
+    Object              string                      `json:"object"`
+    Output              OpenAIModelResponseOutput   `json:"output"`          
 }
 
+type OpenAIModelResponseOutput struct {
+    ID                  string                      `json:"id"`
+    Content             OpenAIModelOutputContent    `json:"output"`     
+    Role                string                      `json:"assitant"`
+    Type                string                      `json:"message"`
+}
+
+type OpenAIModelOutputContent struct {
+    Text                string                      `json:"text"` 
+    Type                string                      `json:"type"` 
+}
